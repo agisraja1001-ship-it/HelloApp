e9d379baf525e545e1cdea9ade38615148fc20c2
Agis <agis@Agis-MacBook-Air-4.local>
2026-03-18 21:05:07 +0530

    uc4

---
+++ src/uc4.java
@@ -0,0 +1,14 @@
+public class uc4 {
+    public static void main(String[] args) {
+        String greeting;
+
+        if (args.length > 0) {
+            String names = String.join(" ", args);
+            greeting = "Hello, " + names + "!";
+        } else {
+            greeting = "Hello, World!";
+        }
+
+        System.out.println(greeting);
+    }
+}
